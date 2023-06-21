2e8a9ceece7eb40d8cd7634234c084ae32f0672a
Prometheys <prometheys112@gmail.com>
2023-06-21 12:57:44 +0300

    First
    1 version

---
+++ Sounds.py
@@ -0,0 +1,41 @@
+from random import *
+from playsound import playsound
+import os
+variable_words = [name.replace('.mp3', '') for name in os.listdir('C:\\Users\\Prometheys\\Desktop\\Python\\Sounds\\')]
+correctguess = 0
+incorrectguess = 0
+mistakes = []
+guess = ''
+choose = int(input('1 for full check, 2 for random check'))
+while choose!= 1 and choose!= 2 :
+    choose = int(input('1 for full check, 2 for random check'))
+if choose == 1:
+    for i in variable_words:
+        playsound('C:\\Users\\Prometheys\\Desktop\\Python\\Sounds\\'+ i + '.mp3')
+        guess = input('Enter your guess or enter "exit" to stop').title()
+        if guess == i:
+            print ('You are right')
+            correctguess+=1
+        elif guess == 'Exit':
+            break
+        else:
+            print ('You are wrong. Right word is '+i)
+            incorrectguess+=1
+            mistakes.append(i)
+    print(mistakes)
+else:
+    while guess != 'Exit':
+        word_random = randint(0,len(variable_words)-1)
+        playsound('C:\\Users\\Prometheys\\Desktop\\Python\\Sounds\\'+ variable_words[word_random] + '.mp3')
+        guess = input('Enter your guess or enter "exit" to stop').title()
+        if guess == variable_words[word_random]:
+            print ('You are right')
+            correctguess+=1
+        elif guess == 'Exit':
+            break
+        else:
+            print ('You are wrong. Right word is '+variable_words[word_random])
+            incorrectguess+=1
+            mistakes.append(variable_words[word_random])
+print('Right answers = ',correctguess, 'Wrong answers = ', incorrectguess)
+print(mistakes)
