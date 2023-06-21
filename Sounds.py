d5500a7a97dc285ffee23e3458eb4e9a3b4f657c
incerteza <tanya_mineeva@bk.ru>
2023-06-21 11:50:11 +0100

    Change absolute directory path to relative
    1) Address the "./Sounds" directory by its relative path
    2) Use os.path.join to get it working on Linus as well

--- Sounds.py
+++ Sounds.py
@@ -1,7 +1,8 @@
 from random import *
 from playsound import playsound
 import os
-variable_words = [name.replace('.mp3', '') for name in os.listdir('C:\\Users\\Prometheys\\Desktop\\Python\\Sounds\\')]
+sounds_dir = os.path.join(os.getcwd(), 'Sounds')
+variable_words = [name.replace('.mp3', '') for name in os.listdir(sounds_dir)]
 correctguess = 0
 incorrectguess = 0
 mistakes = []
@@ -11,7 +12,7 @@ while choose!= 1 and choose!= 2 :
     choose = int(input('1 for full check, 2 for random check'))
 if choose == 1:
     for i in variable_words:
-        playsound('C:\\Users\\Prometheys\\Desktop\\Python\\Sounds\\'+ i + '.mp3')
+        playsound(os.path.join(sounds_dir, i + '.mp3'))
         guess = input('Enter your guess or enter "exit" to stop').title()
         if guess == i:
             print ('You are right')
@@ -26,7 +27,7 @@ if choose == 1:
 else:
     while guess != 'Exit':
         word_random = randint(0,len(variable_words)-1)
-        playsound('C:\\Users\\Prometheys\\Desktop\\Python\\Sounds\\'+ variable_words[word_random] + '.mp3')
+        playsound(os.path.join(sounds_dir, variable_words[word_random] + '.mp3'))
         guess = input('Enter your guess or enter "exit" to stop').title()
         if guess == variable_words[word_random]:
             print ('You are right')
